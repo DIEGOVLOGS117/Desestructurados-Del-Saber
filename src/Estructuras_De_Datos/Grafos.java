b16d4d5e05afcb3266cecd870601f228243028be
Rafael <rafadz@gmail.com>
2025-11-01 21:33:02 -0500

    avance en primer grafo

---
+++ src/Estructuras_De_Datos/Grafos.java
@@ -0,0 +1,15 @@
+
+package Estructuras_De_Datos;
+
+
+public class Grafos {
+    
+    private int id;
+    private String tipo; // si es estudiante o profesor 
+    private String materia;
+    
+    
+    
+    
+    
+}
